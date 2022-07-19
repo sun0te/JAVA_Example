0e46a687754b486877d55a4e9237f5d3780503bf
sun0te <jin1123a@gmail.com>
2022-07-20 00:09:23 +0900

    chapter04_exercise06

--- src/chap04/Exercise06.java
+++ src/chap04/Exercise06.java
@@ -2,10 +2,11 @@ package chap04;	//for 문을 이용하여 * 문자로 직사각형 만들기
 
 public class Exercise06 {
 	public static void main(String[] args) {
-		for(int i = 1; i <=5; i++) {
-			for(int j = 1; j <=i; j++) {
-				System.out.println("*");
+		for(int i = 1; i <= 5; i++) {
+			for(int j = 1; j <= i; j++) {
+		System.out.print("*"); 
 			}
+		
 			System.out.println();
 		}
 	
