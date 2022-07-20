53efc82ef4f682690bdb713e65ebd763de95d138
sun0te <jin1123a@gmail.com>
2022-07-20 15:26:22 +0900

    try(Scanner scanner = new Scanner(System.in))

--- src/chap04/DoWhileExample.java
+++ src/chap04/DoWhileExample.java
@@ -9,14 +9,15 @@ public class DoWhileExample {
 		System.out.println("메시지를 입력하세요. ");
 		System.out.println("프로그램을 종료하려면 q 를 입력하세요. ");
 		
-		Scanner scanner = new Scanner(System.in);//Scanner 객체 생성
-		String inputString;
-		
-		do {
-			System.out.println(">");
-			inputString = scanner.nextLine(); //입력한 문자열 얻음
-			System.out.println(inputString);		
-		} while( ! inputString.equals("q") ); //문자열 비교할 때 equals() 메소드 사용
+		try (Scanner scanner = new Scanner(System.in)) {
+			String inputString;
+			
+			do {
+				System.out.print("> ");
+				inputString = scanner.nextLine(); //입력한 문자열 얻음
+				System.out.println(inputString);		
+			} while( ! inputString.equals("q") ); //문자열 비교할 때 equals() 메소드 사용
+		}
 		// ! : q가 리턴한 boolean 값을 반대로 바꿈, true -> false
 		System.out.println();
 		System.out.println("프로그램 종료");
--- src/chap04/Exercise07.java
+++ src/chap04/Exercise07.java
@@ -9,36 +9,36 @@ public class Exercise07 {
 		boolean run = true;
 		int balance = 0;
 		
-		Scanner scanner = new Scanner(System.in);
-		
-		while(run) {
-			System.out.println("--------------------------");
-			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
-			System.out.println("--------------------------");
-			System.out.println("선택 > ");
-			
-			int num = scanner.nextInt();
-			int money = 0;
-			
-			switch(num) {
-			case 1:
-				System.out.println("예금액 > ");
-				money = scanner.nextInt();
-				balance += money;
-				break;
-			case 2:
-				System.out.println("출급액 > ");
-				money = scanner.nextInt();
-				balance -= money;
-				break;
-			case 3:
-				System.out.println("잔고 > " + balance);
-				break;
-			case 4:
-				run = false;
-				break;
+		try (Scanner scanner = new Scanner(System.in)) {
+			while(run) {
+				System.out.println("--------------------------");
+				System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+				System.out.println("--------------------------");
+				System.out.print("선택 > ");
 				
-			}		
+				int num = scanner.nextInt();
+				int money = 0;
+				
+				switch(num) {
+				case 1:
+					System.out.print("예금액 > ");
+					money = scanner.nextInt();
+					balance += money;
+					break;
+				case 2:
+					System.out.print("출금액 > ");
+					money = scanner.nextInt();
+					balance -= money;
+					break;
+				case 3:
+					System.out.println("잔고 > " + balance);
+					break;
+				case 4:
+					run = false;
+					break;
+					
+				}		
+			}
 		}
 		
 		System.out.println("프로그램 종료");
