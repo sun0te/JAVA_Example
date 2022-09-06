36e2e8401555a23887688c01399ff159ce09890f
sun0te <jin1123a@gmail.com>
2022-09-06 23:24:20 +0900

    commit 246p

---
+++ src/chap06/PersonExample.java
@@ -0,0 +1,17 @@
+package chap06;	//final 필드 테스트
+
+public class PersonExample {
+
+	public static void main(String[] args) {
+	
+		Person p1 = new Person("123456-1234567", "계백");
+		
+		System.out.println(p1.nation);
+		System.out.println(p1.ssn);
+		System.out.println(p1.name);
+		
+		
+		
+	}
+
+}
